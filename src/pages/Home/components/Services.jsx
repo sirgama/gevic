05a6e07a10112570c05cd4ec9b53dfff152d61d2
sirgama <sirengogamaliel@gmail.com>
2022-11-29 09:28:08 +0300

    add content and styling to services section

--- src/pages/Home/components/Services.jsx
+++ src/pages/Home/components/Services.jsx
@@ -4,40 +4,42 @@ export default function Services() {
   return (
     <div>
       <div className="bg-gray-900 mx-auto max-w-screen-2xl py-2 md:py-0 px-2 lg:px-2 lg:py-0 rounded-3xl">
-        <p className="text-orange-800/75 px-2 md:px-20 font-bold py-4">- Our Services</p>
+        <p className="text-orange-800/75 px-2 md:px-20 font-bold py-8">- Our Services</p>
         <h1 className="text-white text-3xl md:text-5xl font-bold px-2 md:px-20 md:py-14 py-4 md:leading-tight">SERVICES WE OFFER</h1>
 
-        <div className="flex flex-row w-full flex-wrap justify-around">
-            <div className=" bg-gray-800/25 rounded-3xl w-1/4 py-6 px-6 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
+        <div className="flex flex-col md:flex-row w-full flex-wrap justify-around">
+            <div className=" bg-gray-800/25 rounded-3xl md:w-1/4 py-6 px-6 md:px-8 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
                 <h1 className="text-5xl font-bold p-4">01</h1>
                 <h3 className=" text-base my-2 font-semibold">Printed and Non printed office stationery</h3>
                 <p className="text-gray-300 font-normal text-sm">We got a huge selections for the Schools and office. Find our extensive range of Diaries, Calendars, adhesives, copier paper, envelopes, exercise books, notepads, pens, whiteboards and much, much more.</p>
             </div>
-            <div className=" bg-gray-800/25 rounded-3xl w-1/4 p-6 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
-                <h1 className="text-5xl font-bold p-4">01</h1>
-                <h3 className=" text-base my-2 font-semibold">Printed and Non printed office stationery</h3>
-                <p className="text-gray-300 font-normal text-sm">We got a huge selections for the Schools and office. Find our extensive range of Diaries, Calendars, adhesives, copier paper, envelopes, exercise books, notepads, pens, whiteboards and much, much more.</p>
+            <div className=" bg-gray-800/25 rounded-3xl md:w-1/4 py-6 px-6 md:px-8 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
+                <h1 className="text-5xl font-bold p-4">02</h1>
+                <h3 className=" text-base my-2 font-semibold">Non pharmaceuticals supplies</h3>
+                <p className="text-gray-300 font-normal text-sm">Non pharmaceuticals range from accessories, bandage and plasters, blood bags, cotton dressing, face masks and surgical caps, first aid kits, gloves, sterile dressing, tubings and clamps, urology, sutures and much more.</p>
             </div>
-            <div className=" bg-gray-800/25 rounded-3xl w-1/4 p-6 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
-                <h1 className="text-5xl font-bold p-4">01</h1>
-                <h3 className=" text-base my-2 font-semibold">Printed and Non printed office stationery</h3>
-                <p className="text-gray-300 font-normal text-sm">We got a huge selections for the Schools and office. Find our extensive range of Diaries, Calendars, adhesives, copier paper, envelopes, exercise books, notepads, pens, whiteboards and much, much more.</p>
+            <div className=" bg-gray-800/25 rounded-3xl md:w-1/4 py-6 px-6 md:px-8 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
+                <h1 className="text-5xl font-bold p-4">03</h1>
+                <h3 className=" text-base my-2 font-semibold">ICT equipments, accessories and consumables</h3>
+                <p className="text-gray-300 font-normal text-sm">Computers, Laptops, accessories Networking devices Such Access points, Switches and Routers Un-interrupted Power Supply (UPS’s) And a host of other products and related peripherals.</p>
             </div>
-            <div className=" bg-gray-800/25 rounded-3xl w-1/4 p-6 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
-                <h1 className="text-5xl font-bold p-4">01</h1>
-                <h3 className=" text-base my-2 font-semibold">Printed and Non printed office stationery</h3>
-                <p className="text-gray-300 font-normal text-sm">We got a huge selections for the Schools and office. Find our extensive range of Diaries, Calendars, adhesives, copier paper, envelopes, exercise books, notepads, pens, whiteboards and much, much more.</p>
+            <div className=" bg-gray-800/25 rounded-3xl md:w-1/4 py-6 px-6 md:px-8 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
+                <h1 className="text-5xl font-bold p-4">04</h1>
+                <h3 className=" text-base my-2 font-semibold">Cleaning materials, laundry soap powder,
+detergents and disinfectants</h3>
+                <p className="text-gray-300 font-normal text-sm">Washing soap, laundry soap, detergent soap, liquid soap, hand wash, liquid detergent, detergent powder, floor cleaner, toilet cleaner, hand sanitizer, napthalene balls, air freshener, hand brooms, mops, hard brush and much more.</p>
             </div>
-            <div className=" bg-gray-800/25 rounded-3xl w-1/4 p-6 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
-                <h1 className="text-5xl font-bold p-4">01</h1>
-                <h3 className=" text-base my-2 font-semibold">Printed and Non printed office stationery</h3>
-                <p className="text-gray-300 font-normal text-sm">We got a huge selections for the Schools and office. Find our extensive range of Diaries, Calendars, adhesives, copier paper, envelopes, exercise books, notepads, pens, whiteboards and much, much more.</p>
+            <div className=" bg-gray-800/25 rounded-3xl md:w-1/4 py-6 px-6 md:px-8 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
+                <h1 className="text-5xl font-bold p-4">05</h1>
+                <h3 className=" text-base my-2 font-semibold">Hotel amenities supplies</h3>
+                <p className="text-gray-300 font-normal text-sm">Shampoo, bath gel, body lotion, soaps, shaving kit, dental kit, sewing kit, vanity kit, bathroom towel, shower cap, hand towel, bed sheets, serviette paper, bathroom robe, chef disposable cap, sanitary bags, shoe shine, shoe mitt, slippers and much more.</p>
             </div>
-            <div className=" bg-gray-800/25 rounded-3xl w-1/4 p-6 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
-                <h1 className="text-5xl font-bold p-4">01</h1>
-                <h3 className=" text-base my-2 font-semibold">Printed and Non printed office stationery</h3>
-                <p className="text-gray-300 font-normal text-sm">We got a huge selections for the Schools and office. Find our extensive range of Diaries, Calendars, adhesives, copier paper, envelopes, exercise books, notepads, pens, whiteboards and much, much more.</p>
+            <div className=" bg-gray-800/25 rounded-3xl md:w-1/4 py-6 px-6 md:px-8 m-2 text-white hover:bg-orange-900  transition ease-in-out duration-150 ">
+                <h1 className="text-5xl font-bold p-4">06</h1>
+                <h3 className=" text-base my-2 font-semibold">General supplies</h3>
+                <p className="text-gray-300 font-normal text-sm">Our company does supplies of office stationery, office electronics, electricals, building materials, cleaning equipment, Plumbing and many more. When it comes to office stationery we really cover a vast array of everyday office items. We supply all major brands and cover the whole spectrum of products.</p>
             </div>
+            
         </div>
       </div>
     </div>
