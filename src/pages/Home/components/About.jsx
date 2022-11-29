492a962c2ef29fee69dbb96ff4d45b9a18865443
sirgama <sirengogamaliel@gmail.com>
2022-11-29 10:46:06 +0300

    add favicon

--- src/pages/Home/components/About.jsx
+++ src/pages/Home/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div>
       <div className='bg-gray-900 mx-auto max-w-screen-2xl py-2 md:py-0 px-2 lg:px-2 lg:py-0 rounded-3xl mt-6'>
       <p className="text-orange-800/75 px-2 md:px-20 font-bold pt-8">- About us</p>
-        <h1 className="text-white text-3xl md:text-5xl font-bold px-2 md:px-20 md:py-8 py-4 md:leading-tight md:w-2/3"><span className="text-orange-900">GEVIC MUNDARE</span> IS ON A JOURNEY TO PROVIDE THE BEST QUALITY SUPPLIES AND A UNIFIED PROCUREMENT PLATFORM </h1>
+        <h1 className="text-white text-3xl md:text-5xl font-bold px-2 md:px-20 md:py-8 py-4 md:leading-tight md:w-11/12"><span className="text-orange-900">GEVIC MUNDARE</span> IS ON A JOURNEY TO PROVIDE THE BEST QUALITY SUPPLIES AND A UNIFIED PROCUREMENT PLATFORM </h1>
       </div>
     </div>
   )
