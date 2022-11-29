9d130cb9f2bfd67170e4f8d3a796ab265517ccad
sirgama <sirengogamaliel@gmail.com>
2022-11-29 03:24:16 +0300

    add hover effect

--- src/pages/Home/pages/Landing.jsx
+++ src/pages/Home/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Navigation from '../components/Navigation'
+import Services from '../components/Services'
 
 export default function Landing() {
   return (
     <div>
       <Navigation />
       <Hero />
+      <Services />
     </div>
   )
 }
